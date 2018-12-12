736cb494eba510c8d12c338f938fde338645d93a
Rocío Jamanca <rozzy.1997@gmail.com>
2018-12-12 23:50:25 +0100

    Segon Commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,22 +11,22 @@ import play.test.Fixtures;
 public class Application extends Controller {
 
     public static void index() {
-        render();
+        renderText("welcome");
     }
 
 
-    public void InicializarBD(){
+    public void InicializarBD() {
         Fixtures.deleteDatabase();
 
         //renderText Base de dades actualizada
-        Diari d1= new Diari("Pais","Català").save();
-        Diari d2= new Diari("Pais","Castella").save();
-        Diari d3= new Diari("Times","Anglès").save();
+        Diari d1 = new Diari("Pais", "Català").save();
+        Diari d2 = new Diari("Pais", "Castella").save();
+        Diari d3 = new Diari("Times", "Anglès").save();
 
-        Subscriptor s1=new Subscriptor("Pedro",15).save();
-        Subscriptor s2=new Subscriptor("Toni",49).save();
-        Subscriptor s3=new Subscriptor("Paula",14).save();
-        Subscriptor s4=new Subscriptor("Carmena",45).save();
+        Subscriptor s1 = new Subscriptor("Pedro", 15).save();
+        Subscriptor s2 = new Subscriptor("Toni", 49).save();
+        Subscriptor s3 = new Subscriptor("Paula", 14).save();
+        Subscriptor s4 = new Subscriptor("Carmena", 45).save();
 
 
         d1.subscriptorList.add(s1);
@@ -54,6 +54,28 @@ public class Application extends Controller {
     }
 
     public void SubscripcionsPerSubscriptor() {
+        List<Subscriptor> subscriptorList = Subscriptor.findAll();
+        List<Diari> diariList = Diari.findAll();
+
+        float subcripcions = 0;
+
+      /*  for (int i=0; i<subscriptorList.size();i++){
+            subcripcions+=subscriptorList.get(i).diariList.size();
+        }*/
+
+        for (int i = 0; i < diariList.size(); i++) {
+            subcripcions += diariList.get(i).subscriptorList.size();
+        }
+        float subs = subscriptorList.size();
+        float result = subcripcions / subs;
+        String res = Float.toString(result);
+        if (subscriptorList.size() == 0 || diariList.size() == 0) {
+            //renderText("-2");
+        } else if (subcripcions == 0) {
+            //renderText("-1");
+        }
+
+        renderText("algo");
 
     }
 }
